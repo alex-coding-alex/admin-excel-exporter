d7a20bc6be9166ed5d9ef78deaf010385e8de520
Alex <alex.l.luong@gmail.com>
2025-04-05 11:06:42 +1030

    Added form submitted notification base

---
+++ app/Notifications/DietFormSubmittedNotification.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class DietFormSubmittedNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct() {}
+
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->greeting('Hello!')
+            ->line('Form has been submitted');
+    }
+
+    public function toArray($notifiable): array
+    {
+        return [];
+    }
+}
